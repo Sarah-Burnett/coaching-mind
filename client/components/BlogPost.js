3041ab5cc4933e49ebef57873f97b7d4a35cc66e
Sarah-Burnett <burnie8657@gmail.com>
2020-09-09 17:48:41 +0100

    dynamic blog route

--- client/components/BlogPost.js
+++ client/components/BlogPost.js
@@ -1,16 +1,23 @@
 import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const Layout = styled.div``;
 
-export default function BlogPost({post}) {
-	console.log(post)
+export default function BlogPost({ post: { body } }) {
 	return (
-		<Layout>
-			<ReactMarkdown
-				source={body}
-				transformImageUri={(input) => `http://localhost:1337${input}`}
-			/>
-		</Layout>
+		<div>
+			<Nav />
+			<main>
+				<Layout>
+					<ReactMarkdown
+						source={body}
+						transformImageUri={(input) => `http://localhost:1337${input}`}
+					/>
+				</Layout>
+			</main>
+			<Footer />
+		</div>
 	);
 }
--- client/pages/blog/[slug].js
+++ client/pages/blog/[slug].js
@@ -3,24 +3,32 @@ import { useState, useEffect } from "react";
 import BlogPost from "../../components/BlogPost";
 
 export default function Post() {
-	// const [loading, setLoading] = useState(true);
-	// const [post, setPost] = useState();
-	// const { slug } = useRouter().query;
-	// console.log(slug);
-	// const fetchPost = async () => {
-	// 	const res = await fetch(`https://localhost:1337/posts?slug=${slug}`);
-    //     const post = await res.json();
-    //     return post;
-    // };
-    // useEffect(() => {
-    //     const post = fetchPost();
-    //     console.log(post);
-    //     setPost(post);
-    // }, [])
-	// return (
-	// 	<div>{loading ? <div>Loading Post </div> : <BlogPost post={post} />}</div>
-    // );
-    return (
-        <div>Blog post</div>
-    )
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [post, setPost] = useState();
+	const { slug } = useRouter().query;
+	const fetchPost = async () => {
+		const res = await fetch(`http://localhost:1337/posts?slug=${slug}`);
+		const post = await res.json();
+		setPost(post[0]);
+	};
+	useEffect(() => {
+		try {
+			fetchPost();
+		} catch {
+			setError(false);
+		}
+		setLoading(false);
+	}, []);
+	return (
+		<div>
+			{loading && <div>Loading Post </div>}
+			{error && <div>Error</div>}
+			{post && <BlogPost post={post} />}
+		</div>
+	);
 }
+
+Post.getInitialProps = async () => {
+	return {};
+};
