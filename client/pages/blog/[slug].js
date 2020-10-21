d3317535928753cf8e32155d6e5796fc4bb311a2
Sarah-Burnett <burnie8657@gmail.com>
2020-10-21 16:42:28 +0100

    blog pages

--- client/pages/blog/[slug].js
+++ client/pages/blog/[slug].js
@@ -1,34 +1,24 @@
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 import BlogPost from "../../components/BlogPost";
 
-export default function Post() {
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(false);
-	const [post, setPost] = useState();
-	const { slug } = useRouter().query;
-	const fetchPost = async () => {
-		const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/posts?slug=${slug}`);
-		const post = await res.json();
-		setPost(post[0]);
-	};
-	useEffect(() => {
-		try {
-			fetchPost();
-		} catch {
-			setError(false);
-		}
-		setLoading(false);
-	}, []);
-	return (
-		<>
-			{loading && <div>Loading Post </div>}
-			{error && <div>Error</div>}
-			{post && <BlogPost post={post} />}
-		</>
-	);
+export default function Post({ post }) {
+	return <BlogPost post={post} />;
 }
 
-Post.getInitialProps = async () => {
-	return {};
-};
+export async function getStaticPaths() {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/posts`);
+	const posts = await res.json();
+	const paths = posts.map((post) => ({ params: { slug: post.slug } }));
+	return {
+		paths,
+		fallback: false,
+	};
+}
+
+export async function getStaticProps({ params }) {
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_STRAPI_URL}/posts?slug=${params.slug}`
+	);
+	const posts = await res.json();
+	const post = posts[0];
+	return { props: { post } };
+}
