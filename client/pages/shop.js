c693d43c1e2ca039923169329c760e47e395891d
Sarah-Burnett <burnie8657@gmail.com>
2020-11-10 17:29:41 +0000

    fetch products

--- client/pages/shop.js
+++ client/pages/shop.js
@@ -1,23 +1,32 @@
-import { productsQuery, shopQuery } from "../utilities/queries";
+import { productsQuery as query } from "../utilities/queries";
 
-export default function Shop({products}) {
-	console.log(products);
-	return (<div>SHOP MATE</div>);
+export default function Shop({ products }) {
+	console.log(products[0].node.images);
+	return (
+		<div>
+			{products.map(({ node }) => (
+				<div key={node.id}>
+					<h1>{node.title}</h1>
+					<img src={node.images.edges[0].node.src} width="200" />
+					<p>{node.description}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export async function getStaticProps() {
-	console.log(process.env.NEXT_PUBLIC_SHOPIFY_URL);
 	const data = await fetch(process.env.NEXT_PUBLIC_SHOPIFY_URL, {
 		method: "POST",
 		headers: {
 			"X-Shopify-Storefront-Access-Token":
 				process.env.NEXT_PUBLIC_SHOPIFY_TOKEN,
 			Accept: "application/json",
-			"Content-Type": "application/graphql",
+			"Content-Type": "application/json",
 		},
-		body: JSON.stringify(shopQuery),
+		body: JSON.stringify({ query }),
 	});
-	const products = await data.json();
-	console.log(products);
+	let products = await data.json();
+	products = products.data.products.edges;
 	return { props: { products } };
 }
